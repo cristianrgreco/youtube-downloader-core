b22320a107c3d0f30d9647b6c9ebd9cc9dea977a
Cristian Greco <cristianrgreco@gmail.com>
2018-02-10 20:08:43 +0000

    Minor refactoring

--- src/youtube-downloader.js
+++ src/youtube-downloader.js
@@ -1,36 +1,30 @@
 const {spawn} = require('child_process')
 const {youtubeBinaryPath} = require('./conf')
 
-const getTitle = url => new Promise(async (resolve, reject) => {
-  const process = spawn(youtubeBinaryPath, [
+const getTitle = url => {
+  return streamProcess(spawn(youtubeBinaryPath, [
     '--get-title',
     '--encoding', 'UTF-8',
     '--no-part',
     '--no-playlist',
     url
-  ])
+  ]))
+}
 
-  const {stdout, stderr, hasError} = await streamProcess(process)
-
-  if (hasError) {
-    return reject(stderr)
-  } else {
-    return resolve(stdout)
-  }
-})
-
-const streamProcess = process => new Promise(resolve => {
+const streamProcess = process => new Promise((resolve, reject) => {
   const outs = []
   process.stdout.on('data', data => outs.push(data))
 
   const errs = []
   process.stderr.on('data', data => errs.push(data))
 
-  return process.on('close', () => resolve({
-    stdout: outs.join('').trim(),
-    stderr: errs.join('').trim(),
-    hasError: errs.length > 0
-  }))
+  return process.on('close', () => {
+    if (errs.length > 0) {
+      return reject(errs.join('').trim())
+    } else {
+      return resolve(outs.join('').trim())
+    }
+  })
 })
 
 module.exports = {
