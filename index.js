ef5779f7daa3f437bffa869a2dade446febaadff
Cristian Greco <cristianrgreco@gmail.com>
2018-02-11 14:42:16 +0000

    Create main export

---
+++ index.js
@@ -0,0 +1,13 @@
+const {
+  getTitle,
+  getFilename,
+  downloadVideo,
+  downloadAudio
+} = require('./src/youtube-downloader')
+
+module.exports = {
+  getTitle,
+  getFilename,
+  downloadVideo,
+  downloadAudio
+}
