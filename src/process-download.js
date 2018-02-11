fa994e5ccbe4e320aba44a7b7526efc78f64c095
Cristian Greco <cristianrgreco@gmail.com>
2018-02-11 13:57:00 +0000

    Wrap mutable state in closure

--- src/process-download.js
+++ src/process-download.js
@@ -12,24 +12,25 @@ const processDownload = process => {
   process.stdout.setEncoding('UTF-8')
   process.stderr.setEncoding('UTF-8')
 
-  const emitter = new EventEmitter()
+  const emitter = new EventEmitter();
 
-  let currentState = states.NONE
-  byline(process.stdout).on('data', data => {
-    const newState = getState(data)
+  (currentState => {
+    byline(process.stdout).on('data', data => {
+      const newState = getState(data)
 
-    if (newState !== null && newState !== currentState) {
-      currentState = newState
-      logger.debug('emitting state event: %o', currentState)
-      emitter.emit('state', currentState)
-    }
+      if (newState !== null && newState !== currentState) {
+        currentState = newState
+        logger.debug('emitting state event: %o', currentState)
+        emitter.emit('state', currentState)
+      }
 
-    if (newState === states.DOWNLOADING) {
-      const progress = getProgress(data)
-      logger.debug('emitting progress event: %o', progress)
-      emitter.emit('progress', progress)
-    }
-  })
+      if (newState === states.DOWNLOADING) {
+        const progress = getProgress(data)
+        logger.debug('emitting progress event: %o', progress)
+        emitter.emit('progress', progress)
+      }
+    })
+  })(states.NONE)
 
   const errs = []
   byline(process.stderr).on('data', data => errs.push(data))
